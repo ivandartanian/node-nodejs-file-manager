684fc0bf5caae93443f0b232fd9f383e28ad2995
ivandartanian <tukituk1435@gmail.com>
2022-12-19 04:04:24 +0300

    completed 5 tasked

--- src/index.js
+++ src/index.js
@@ -1,26 +1,54 @@
 import * as path from 'path';
-import { fileURLToPath } from 'url';
+import * as fs from 'fs'
+
+const {promises} = fs;
 
 const getUserName = () => process.argv.slice(2)[0].slice(11);
+const currentDirMessage = () => process.stdout.write(`You are currently in ${process.cwd()}\n`);
+const byeMessage = () => process.stdout.write('Thank you for using File Manager, Username, goodbye!\n');
 
 const userName = getUserName();
-process.stdout.write(`Welcome to the File Manager, ${userName}!\n`);
-process.stdout.write(`You are currently in ${path.dirname(fileURLToPath(import.meta.url))}\n`);
+const rootDir = process.cwd();
+// const rootDir = path.resolve('./');
+// const prevRootDir = path.resolve('../');
 
-const echoInput = (chunk) => {
-    const chunkStringified = chunk.toString();
-    process.stdout.write(`You are currently in ${path.dirname(fileURLToPath(import.meta.url))}\n`);
-    if (chunkStringified.includes('.exit')) {
-        process.stdout.write('Thank you for using File Manager, Username, goodbye!');
+process.stdout.write(`Welcome to the File Manager, ${userName}!\n`);
+currentDirMessage();
+
+const echoInput = async (chunk) => {
+    const chunkStringified = chunk.toString().trim();
+    const chunkArr = chunkStringified.split(' ');
+    if (chunkStringified === '.exit') {
+        byeMessage();
         process.exit(0);
     };
-};
+    if(chunkStringified === 'up' /* && prevRootDir !== path.resolve('../') */) {
+        process.chdir('../');
+    };
+    if(chunkArr[0] === 'cd' && chunkArr[1]) {
+        process.chdir(chunkArr[1]);
+    };
+    if(chunkStringified === 'ls') {
+        const filesArr = [];
+
+        const files = await promises.readdir(process.cwd());
+        for await (const file of files) {
+            const stat = await promises.stat(file);
+            filesArr.push({Name: file, Type: stat.isFile() ? 'file' : 'directory'});
+        }
+        console.table(filesArr);
+    }
+         
+    currentDirMessage();
+}; 
 
 process.stdin.on('data', echoInput);
 process.on('SIGINT', () => {
-    process.stdout.write(`You are currently in ${path.dirname(fileURLToPath(import.meta.url))}\n`);
-    process.stdout.write('Thank you for using File Manager, Username, goodbye!');
+    currentDirMessage();
+    byeMessage();
     process.exit(0);
 }
 );
+
+
 
